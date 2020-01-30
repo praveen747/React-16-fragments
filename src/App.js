340bb1f9cdb971b1f97ca37aea14f149c5ed8de3
Praveenkumar Padamati <36552399+praveen747@users.noreply.github.com>
2020-01-30 11:21:48 -0600

    import fragment
    <>
     Way to write fragment
    </>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.css";
 
 export default function App() {
-  return [
-    "Writing React-16: Fragmenets",
-    <h2 key="heading-1"> Fragments Feature</h2>,
-    "Returning an Array of Elements from a components render Method",
-    <h2 key="heading-2"> Rules to achieve this</h2>,
-    "Children in an array must be separated by commas",
-    "Children in an array must have a key to prevent React’s key warning.",
-    "Strings must be wrapped in quotes."
-  ];
+  return (
+    <>
+      "Writing React-16: Fragmenets"
+      <h2 key="heading-1"> Fragments Feature</h2>, "Returning an Array of
+      Elements from a components render Method",
+      <h2 key="heading-2"> Rules to achieve this</h2>, "Children in an array
+      must be separated by commas", "Children in an array must have a key to
+      prevent React’s key warning.", "Strings must be wrapped in quotes."
+    </>
+  );
 }
